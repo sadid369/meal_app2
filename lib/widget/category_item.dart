fb691002ae9efe6f563031a3c14259da6b690266
Sadid369 <sadid.jones@gmail.com>
2021-07-31 19:54:14 +0600

    day 1 completed

---
+++ lib/widget/category_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  CategoryItem({required this.title, required this.color});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
+      ),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            end: Alignment.bottomRight,
+            begin: Alignment.topLeft,
+          ),
+          borderRadius: BorderRadius.circular(15)),
+    );
+  }
+}
