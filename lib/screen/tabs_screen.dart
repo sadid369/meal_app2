b782d88f3e72d715b875c8c9d1c50d700c12c4af
Sadid369 <sadid.jones@gmail.com>
2021-08-08 06:32:21 +0600

    day 8 BottomNavigationBar

--- lib/screen/tabs_screen.dart
+++ lib/screen/tabs_screen.dart
@@ -8,33 +8,52 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, dynamic>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Category',
+    },
+    {
+      'page': FavoriteScreen(),
+      'title': 'Your Favorites',
+    }
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectedPages(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Category',
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: 'Favorite',
-              )
-            ],
-          ),
+          title: Text(_pages[_selectedPageIndex]['title']),
         ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreen(),
+        body: _pages[_selectedPageIndex]['page'],
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Theme.of(context).accentColor,
+          unselectedItemColor: Colors.white,
+          currentIndex: _selectedPageIndex,
+          type: BottomNavigationBarType.shifting,
+          onTap: _selectedPages,
+          backgroundColor: Theme.of(context).primaryColor,
+          items: [
+            BottomNavigationBarItem(
+                backgroundColor: Theme.of(context).primaryColor,
+                icon: Icon(
+                  Icons.category,
+                ),
+                title: Text('Category')),
+            BottomNavigationBarItem(
+                backgroundColor: Theme.of(context).primaryColor,
+                icon: Icon(
+                  Icons.star,
+                ),
+                title: Text('Favorite')),
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
